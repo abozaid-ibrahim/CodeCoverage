e02df568f55e99d4cf7967c480988b1442b09e84
abuzeid <abozaid.hamed@vodafone.com>
2018-11-19 11:35:57 +0200

    writing two dummy test case

--- CodeCoverage/ViewController.swift
+++ CodeCoverage/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print(addTwoNums(n1: 4, n2: 5))
+        
     }
-
-
+    
+    func addTwoNums(n1:Int, n2:Int)->Int{
+        return n1 + n2
+    }
+    
+    func multipleDigits(n:Int...)->Int{
+        var result = 1
+        n.forEach{item in
+            result = item * result
+        }
+        return result
+    }
 }
 
